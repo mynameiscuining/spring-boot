7631dd433f30c73617587197a76122a395154077
cuining <cuining@asiainfo-sec.com>
2019-05-07 17:08:09 +0800

    初始化

---
+++ spring-boot-basic/src/main/java/cn/njyazheng/springboot/Application.java
@@ -0,0 +1,14 @@
+package cn.njyazheng.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+public class Application {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
