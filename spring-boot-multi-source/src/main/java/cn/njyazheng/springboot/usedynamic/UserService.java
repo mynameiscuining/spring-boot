2a2b5f19120366440190e7e83f325861dff8bec9
cuining <cuining@asiainfo-sec.com>
2019-12-12 15:45:08 +0800

    修改说明

--- spring-boot-multi-source/src/main/java/cn/njyazheng/springboot/usedynamic/UserService.java
+++ spring-boot-multi-source/src/main/java/cn/njyazheng/springboot/usedynamic/UserService.java
@@ -8,26 +8,27 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@DS("db02")
+@DS("db01")
 public class UserService {
-    
+
     @Autowired
     private UserMapper userMapper;
+
     //注解方法优先
     @DS("db01")
-    public boolean addUserDB01(User user){
-       userMapper.insert(user);
-       return true;
-    }
-    
-    @DS("db02")
-    public boolean addUserDB02(User user){
+    public boolean addUserDB01(User user) {
         userMapper.insert(user);
         return true;
     }
 
     @DS("db02")
-    public User selectUser(){
+    public boolean addUserDB02(User user) {
+        userMapper.insert(user);
+        return true;
+    }
+
+    @DS("db02")
+    public User selectUser() {
         return userMapper.selectByPrimaryKey(1);
     }
 
@@ -35,14 +36,14 @@ public class UserService {
      * 同一个数据源会回滚
      */
     @Transactional
-    public void insertSingleSource(){
-        User user=new User();
+    public void insertSingleSource() {
+        User user = new User();
         user.setId(2);
         user.setAge(1);
         user.setName("lily");
         addUserDB01(user);
         selectUser();
-        user=new User();
+        user = new User();
         user.setId(1);
         user.setName("lucy");
         addUserDB01(user);
@@ -50,23 +51,24 @@ public class UserService {
     }
 
     /**
-     * 不同数据源异常会回滚
+     * 子方法@Ds会失效,数据源为同一个
      */
     @Transactional
-    public void insertDiffSource(){
-        User user=new User();
-        user.setName("lily");
-        user.setAge(1);
-        user.setId(22);
-        addUserDB01(user);
+    public void insertDiffSource() {
+        User user = null;
         selectUser();
-        user=new User();
+        user = new User();
         user.setName("lucy");
         user.setAge(1);
         addUserDB02(user);
-        user=new User();
+        user = new User();
         user.setName("lilei");
+        user.setAge(1);
         addUserDB02(user);
-
+        user = new User();
+        user.setName("lily");
+        user.setId(22);
+        user.setAge(1);
+        addUserDB01(user);
     }
 }
